4a95848f1fa2408db21dd7fb8e8274f5a28434e6
AsaduzzamanSohun <asaduzzamansohun5@gmail.com>
2024-04-24 18:33:45 +0600

    through get and post ops data stored from client side to server side

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ const cors = require('cors')
 const app = express();
 require('dotenv').config();
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
 
 app.use(cors());
 app.use(express.json());
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
 const uri = `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.k7dzav4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 
@@ -26,15 +26,31 @@ async function run() {
         // Connect the client to the server	(optional starting in v4.7)
         await client.connect();
         // Send a ping to confirm a successful connection
+
+        const coffeeCollection = client.db('espressoDB').collection('espresso');
+
+        app.get('/coffees', async(req, res) => {
+            const cursor = coffeeCollection.find()
+            const result = await cursor.toArray();
+            res.send(result)
+        })
+
+
+        app.post('/coffees', async(req, res) => {
+            const coffeeDetails = req.body;
+            const result = await coffeeCollection.insertOne(coffeeDetails);
+            res.send(result);
+        })
+
+
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
-        await client.close();
+        // await client.close();
     }
 }
 run().catch(console.dir);
-
 
 
 app.get('/', (req, res) => {
